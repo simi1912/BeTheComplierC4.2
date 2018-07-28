11489fc7189e05686bd50df6605b12d25e93c525
Simi <nioasca@gmail.com>
2018-07-28 11:34:38 +0300

    Initial commit

---
+++ src/Hobbits.java
@@ -0,0 +1,23 @@
+public class Hobbits {
+    String name;
+
+    public static void main (String [] args) {
+        Hobbits [] h = new Hobbits[3];
+        int z=0;
+
+        while(z<3) {
+            h[z] = new Hobbits();
+            if (z==0) {
+                h[z].name="bilbo";
+            }
+            if (z==1) {
+                h[z].name="frodo";
+            }
+            if (z==2) {
+                h[z].name="sam";
+            }
+            System.out.println(h[z].name + " is a good hobbit name");
+            z++;
+        }
+    }
+}
